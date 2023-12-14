19d518d063f842be2144e3e3e7c907c6a9fd49ea
Maryam Badghisi <zcbtadg@ucl.ac.uk>
2023-12-14 23:39:14 +0500

    javasript

---
+++ script.js
@@ -0,0 +1,17 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addTask() {
+    if(inputBox.value == ' '){
+        alert("Please enter a task");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = dpcument.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = " ";
+}
